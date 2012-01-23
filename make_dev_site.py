6150b0bc671ad190e0e138bd19ebce5820b27543
bryan kennedy <bkennedy@smm.org>
2012-01-23 14:47:11 -0600

    Switching to the argparse method to read options

--- make_dev_site.py
+++ make_dev_site.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python
 # make_dev_site.py
-import sys
 
-for arg in sys.argv:
-    print arg
+import subprocess
+import logging
+import argparse
 
-#def makeDevSite(args):
-    #""" Clone a site and its database
-    #"""
+def main():
 
-#if __name__ = "__main__":
-    #print makeDevSite(args)
+    parser = argparse.ArgumentParser(description='Make a dev site.')
+    parser.add_argument('giturl',
+            help='URL of the git repo.')
+    parser.add_argument('destination',
+            help='Path to a destination for your clone')
 
+    parser.add_argument('-b', '--branch', default='master',
+            help='Git branch to clone')
+
+# Have this set a default to the git's repo name after the repo is fetched
+    #parser.add_argument('-n', '--name', default=parser.parse_args().giturl.repo_name,
+            #help='Git branch to clone')
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
