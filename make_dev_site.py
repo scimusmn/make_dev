afbcc5af5b90ed4a719076f3d55b3e5a0d839b05
bryan kennedy <bkennedy@smm.org>
2012-01-31 11:56:39 -0600

    Adding a yes/no prompt function
    This is a common python recipe for asking for Yes/No input from users.
    We'll use it to prompt people at trees in the git process.

--- make_dev_site.py
+++ make_dev_site.py
@@ -78,5 +78,29 @@ def call_command(command):
 # Returns a tuple of the stdout and stderr
     return process.communicate()
 
+# Query users for a yes or no response
+def query_yes_no(question, default="yes"):
+    valid = {"Yes":True: "y":True: "ye":True:
+            "No":False, "n":False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("Invalid default answer '%s'" % default)
+
+    while True:
+        sys.stdout.write( question + prompt )
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
 if __name__ == "__main__":
     main()
