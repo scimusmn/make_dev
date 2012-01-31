6efd9f6f767974af4b5a7bf01abd015a702fd299
bryan kennedy <bkennedy@smm.org>
2012-01-31 16:14:57 -0600

    Indentation style

--- make_dev_site.py
+++ make_dev_site.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('-b', '--branch', default='master',
                         help='Git branch to clone')
 
-# Have this set a default to the git's repo name after the repo is fetched
+    # Have this set a default to the git's repo name after the repo is fetched
     #parser.add_argument('-n', '--name', default=parser.parse_args().giturl.repo_name,
             #help='Git branch to clone')
 
